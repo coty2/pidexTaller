4ff7d88c7e72a3d4aa76f920627ed3c84daa27c5
coty2 <cotybenedetti5@gmail.com>
2025-07-07 16:02:09 -0300

    Se agrego para que funcione la api

---
+++ src/common/constants.ts
@@ -0,0 +1,25 @@
+export const pixdexColors = {
+  fondo: "#1A1F2C",
+  purpura: "#6E59A5",
+  purpuraOscuro: "#4A3D70",
+  purpuraClaro: "#9B87F5",
+  verde: "#5FD068",
+  verdeOscuro: "#3A8041",
+  grisOscuro: "#403E43",
+  grisClaro: "#F6F6F7",
+  rojo: "#FF6B6B",
+};
+
+export const API_URL = "http://192.168.1.14:8081";
+
+// Configuración del juego
+export const GAME_CONFIG = {
+  VIDAS_INICIALES: 5,
+  PUNTOS_POR_ADIVINANZA: 1,
+  TIEMPO_LIMITE: 300, // 5 minutos en segundos
+};
+
+export const APP_CONFIG = {
+  NOMBRE: "PidexTaller",
+  VERSION: "1.0.0",
+};
