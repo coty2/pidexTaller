cb98cb1c789e82c8e95181df0000494102add43b
coty2 <cotybenedetti5@gmail.com>
2025-07-07 15:58:25 -0300

    optimizacion de codigo en detail

--- app/detail/[id].tsx
+++ app/detail/[id].tsx
@@ -2,51 +2,54 @@
 import Badge from '@/src/components/Badge';
 import { TextPressStart2P } from '@/src/components/TextPressStart2P';
 import { contenidosAudiovisuales } from '@/src/data/contenidosAudiovisuales';
-import { useLocalSearchParams, useRouter } from 'expo-router';
+import { tiposContenidoAudiovisual } from '@/src/data/tiposContenidoAudiovisual';
+import { useLocalSearchParams } from 'expo-router';
 import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-
-
 export default function DetailScreen() {
     const { id } = useLocalSearchParams();
-    const router = useRouter();
     
     const content = contenidosAudiovisuales.find(c => c.id === Number(id));
 
+    if (!content) {
+        return (
+            <SafeAreaView style={styles.container}>
+                <Text style={styles.errorText}>Contenido no encontrado</Text>
+            </SafeAreaView>
+        );
+    }
 
-if (!content) {
+    const typeContent = tiposContenidoAudiovisual.find(t => t.id === content.tipoId);
+
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.errorText}>Contenido no encontrado</Text>
+            <ScrollView style={styles.content}>
+                <View style={styles.imageContainer}>
+                    <Image 
+                        source={{ uri: String(content.imageUrl) }} 
+                        style={styles.image}
+                        resizeMode="cover"
+                    />
+                </View>
+                <View style={styles.info}>
+                    <TextPressStart2P style={styles.title}>{content.nombre}</TextPressStart2P>
+                    {typeContent && (
+                        <View style={styles.typeContainer}>
+                            <Badge label={typeContent.singular} />
+                        </View>
+                    )}
+                    <Text style={styles.description}>{content.descripcion}</Text>
+                    <TextPressStart2P style={styles.genre}>Genres</TextPressStart2P>
+                    <View style={styles.genresContainer}>
+                        {content.generos.map((genreId) => (
+                            <Badge key={genreId} genreId={genreId}/>
+                        ))}
+                    </View>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     );
-}
-
-return (
-    <SafeAreaView style={styles.container}>
-        <ScrollView style={styles.content}>
-            <View style={styles.imageContainer}>
-                <Image 
-                    source={{ uri: content.imageUrl }} 
-                    style={styles.image}
-                    resizeMode="cover"
-                />
-            </View>
-            <View style={styles.info}>
-                <TextPressStart2P style={styles.title}>{content.nombre}</TextPressStart2P>
-                <Text style={styles.description}>{content.descripcion}</Text>
-                <TextPressStart2P style={styles.genre}>Genres</TextPressStart2P>
-            <View style={styles.genresContainer}>
-                {content.generos.map((genreId) => (
-                    <Badge key={genreId} genreId={genreId} />
-                ))}
-
-            </View>
-            </View>
-        </ScrollView>
-    </SafeAreaView>
-);
 }
 
 const styles = StyleSheet.create({
@@ -54,20 +57,14 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#151521',
     },
-    header: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        padding: 16,
-        backgroundColor: '#1E1E2E',
-    },
     content: {
         borderWidth: 4,
         borderColor: "#403E43",
-        paddingTop: 16,
-        paddingBottom: 8,
-        position: "relative", 
-        marginBottom: 24,
+        paddingTop: 8,
+        paddingBottom: 24,
         paddingHorizontal: 16,
+        marginBottom: 20,
+        minHeight: 700,
     },
     imageContainer: {
         width: '100%',
@@ -79,29 +76,28 @@ const styles = StyleSheet.create({
         height: '100%',
     },
     info: {
-        padding: 16,
+
     },
     title: {
         fontSize: 20,
         color: '#6E59A5',
-        marginBottom: 16,
+        marginTop: 15,
+        marginBottom: 5,
     },
     description: {
         color: '#FFFFFF',
-        marginBottom: 16,
+        marginBottom: 10,
+    },
+    typeContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        gap: 8,
+        marginBottom: 5,
     },
     genresContainer: {
         flexDirection: 'row',
         flexWrap: 'wrap',
         gap: 8,
-    },
-    genreTag: {
-        backgroundColor: '#403E43',
-        padding: 5,
-    },
-    genreText: {
-        fontSize: 10,
-        color: '#FFFFFF',
     },
     errorText: {
         fontFamily: 'VT323',
@@ -110,12 +106,9 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         marginTop: 20,
     },
-    genre:{
+    genre: {
         fontSize: 14,
         color: "#5FD068",
-        marginBottom: 4,
-    },
-    icon: {
-        marginRight: 4,
+        marginBottom: 5,
     },
 });
